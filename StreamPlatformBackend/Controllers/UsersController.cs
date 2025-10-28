db3bc1b82427ddaec88f57f93fe5fecd0feea65e
TeaMoon <assassenswot@gmail.com>
2025-10-28 18:27:32 +0300

    add api route

--- StreamPlatformBackend/Controllers/UsersController.cs
+++ StreamPlatformBackend/Controllers/UsersController.cs
@@ -11,7 +11,7 @@ using System.Security.Claims;
 namespace StreamPlatformBackend.Controllers
 {
     [ApiController]
-
+    [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
 
