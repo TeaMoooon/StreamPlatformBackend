a94e7b3adb10f9534bd14e65fee7b35142ef23fa
TeaMoon <assassenswot@gmail.com>
2025-12-22 14:16:17 +0300

    edit livetranscoder for better work

--- StreamPlatformBackend/Services/LiveTranscoderService.cs
+++ StreamPlatformBackend/Services/LiveTranscoderService.cs
@@ -49,21 +49,21 @@ namespace StreamPlatformBackend.Services
 -map 0:v -map 0:a
 -c:v:0 libx264 -preset veryfast -tune zerolatency -s 1920x1080 -b:v:0 6000k
 -c:a:0 aac -b:a:0 160k
--f hls -hls_time 2 -hls_list_size 6 -hls_flags delete_segments
+-f hls -hls_time 2 -hls_list_size 10 -hls_flags delete_segments+append_list
 -hls_segment_filename ""{baseDir}/1080p/index_%03d.ts""
 ""{baseDir}/1080p/index.m3u8""
 
 -map 0:v -map 0:a
 -c:v:1 libx264 -preset veryfast -tune zerolatency -s 1280x720 -b:v:1 3000k
 -c:a:1 aac -b:a:1 128k
--f hls -hls_time 2 -hls_list_size 6 -hls_flags delete_segments
+-f hls -hls_time 2 -hls_list_size 10 -hls_flags delete_segments+append_list
 -hls_segment_filename ""{baseDir}/720p/index_%03d.ts""
 ""{baseDir}/720p/index.m3u8""
 
 -map 0:v -map 0:a
 -c:v:2 libx264 -preset veryfast -tune zerolatency -s 854x480 -b:v:2 1500k
 -c:a:2 aac -b:a:2 96k
--f hls -hls_time 2 -hls_list_size 6 -hls_flags delete_segments
+-f hls -hls_time 2 -hls_list_size 10 -hls_flags delete_segments+append_list
 -hls_segment_filename ""{baseDir}/480p/index_%03d.ts""
 ""{baseDir}/480p/index.m3u8""
 ";
